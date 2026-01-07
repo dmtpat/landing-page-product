73668b3568b25a8045a765b731d30295fb568cee
Pat <dmtpatricica@email.com>
2026-01-07 22:19:10 +0100

    section 1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./main.module.css"
 import invisible from "../../assets/invisible.png"
 import Jumbotron from "./Jumbotron";
 function Main() {
@@ -8,17 +9,30 @@ function Main() {
             <div className='container'>
                 <div className="row">
                     <div className="col-4 container text-center">
-                        <div className="row"><div className="col"></div></div>
-                        <div className="row"><div className="col"></div></div>
-                        <div className="row"><div className="col"></div></div>
+                        <div className="row"><div className="col">
+                            <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsa iste doloremque, dolorum tempora, aliquid error amet fugit laboriosam, possimus nulla recusandae? Eum obcaecati vitae cupiditate sed excepturi qui aliquam.</p></div></div>
+                        <div className="row"><div className="col">
+                            <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsa iste doloremque, dolorum tempora, aliquid error amet fugit laboriosam, possimus nulla recusandae? Eum obcaecati vitae cupiditate sed excepturi qui aliquam.</p></div></div>
+                        <div className="row"><div className="col">
+                            <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsa iste doloremque, dolorum tempora, aliquid error amet fugit laboriosam, possimus nulla recusandae? Eum obcaecati vitae cupiditate sed excepturi qui aliquam.</p></div></div>
                     </div>
                     <div className="col-4">
-                        <img src={invisible} alt="" />
+                        <img className='img-thumbnail border-0 my-auto p-2' src={invisible} alt="" />
                     </div>
                     <div className="col-4 container text-center">
-                        <div className="row"><div className="col"></div></div>
-                        <div className="row"><div className="col"></div></div>
-                        <div className="row"><div className="col"></div></div>
+                        <div className="row"><div className="col">
+                            <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsa iste doloremque, dolorum tempora, aliquid error amet fugit laboriosam, possimus nulla recusandae? Eum obcaecati vitae cupiditate sed excepturi qui aliquam.</p>
+                        </div></div>
+                        <div className="row"><div className="col">
+                            <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsa iste doloremque, dolorum tempora, aliquid error amet fugit laboriosam, possimus nulla recusandae? Eum obcaecati vitae cupiditate sed excepturi qui aliquam.</p></div></div>
+                        <div className="row"><div className="col">
+                            <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ipsa iste doloremque, dolorum tempora, aliquid error amet fugit laboriosam, possimus nulla recusandae? Eum obcaecati vitae cupiditate sed excepturi qui aliquam.</p></div></div>
                     </div>
                 </div>
             </div>
