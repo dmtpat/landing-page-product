885001f4fc8ce7bf851d4772bd51fb7614bd0b31
Pat <dmtpatricica@email.com>
2026-01-11 04:09:56 +0100

    shop fixed

--- src/components/Main/ShopSection.components.jsx
+++ src/components/Main/ShopSection.components.jsx
@@ -5,9 +5,9 @@ import ShopCard from './ShopCard.components';
 
 function ShopSection() {
     return (
-        <div id='paperelle' className='bg-light text-center'>
-            <h2 className='fs-1'>La collezione di paperelle</h2>
-            <p>Una paperella per ogni stile di programmazione</p>
+        <div id='paperelle' className='bg-light text-center my-5 py-5'>
+            <h2 className=' display-2 fw-semibold pt-5'>La collezione di paperelle</h2>
+            <p className='fs-5 pb-4'>Una paperella per ogni stile di programmazione</p>
             <div className="container text-start">
                 <div className="row ">
                     {Ducks.map((duck) => (
