e6c36aa16326ab836b54ce1edf85567fb3587106
Pat <dmtpatricica@email.com>
2026-01-12 21:44:14 +0100

    header style fixed FIXED

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
+import style from './header.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import logo from '../../assets/logo.png'
 import Nav from 'react-bootstrap/Nav'
-import './header.module.css'
 
 function Header() {
     return (
@@ -20,34 +20,34 @@ function Header() {
                         <div className="collapse navbar-collapse " id="navbarSupportedContent">
                             <ul className="nav nav-pills navbar-nav me-auto mb-2 mb-lg-0 ">
                                 <li className="nav-item">
-                                    <a className="nav-link navLink " href="#home">Home</a>
+                                    <a className={'nav-link ' + style.navLink} href="#home">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link navLink" href="#punti_di_forza">I punti di forza</a>
+                                    <a className={'nav-link ' + style.navLink} href="#punti_di_forza">I punti di forza</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link navLink" href="#recensioni">Recensioni</a>
+                                    <a className={'nav-link ' + style.navLink} href="#recensioni">Recensioni</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link navLink" href="#paperelle">Paperelle</a>
+                                    <a className={'nav-link ' + style.navLink} href="#paperelle">Paperelle</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link navLink" href="#faqs">FAQs</a>
+                                    <a className={'nav-link ' + style.navLink} href="#faqs">FAQs</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link navLink" href="#newsletter">Newsletter</a>
+                                    <a className={'nav-link ' + style.navLink} href="#newsletter">Newsletter</a>
                                 </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </Nav>
+                            </ul >
+                        </div >
+                    </div >
+                </div >
+            </Nav >
             <div className="container-fluid  ">
                 <a className="navbar-brand" href="#">
                     <img src={logo} alt="Rubber Duck" />
                 </a>
             </div>
-        </header>
+        </header >
 
     );
 }
