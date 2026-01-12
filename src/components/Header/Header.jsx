23c8950fc98123a7254925f43cc371077a595432
Pat <dmtpatricica@email.com>
2026-01-12 19:17:59 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import './Header.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import logo from '../../assets/logo.png'
 import Nav from 'react-bootstrap/Nav'
+import './header.module.css'
 
 function Header() {
     return (
